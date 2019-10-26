8bebcdf28de8abf34075ae2a2c462078e06c6d55
MilenaGiachetti <giachettimilena@gmail.com>
2019-10-26 17:36:40 -0300

    Se eliminó el espacio entre lineas, se agrandó tamaño de celdas a 50px y se agregó animación

--- principal.js
+++ principal.js
@@ -1,25 +1,31 @@
-
+var btnTablero = document.getElementById('btnTablero');
 var tablero = document.getElementById('tablero');
 var num = 1;
 
 function crearTablero(){
     while (num <= 72){
         if (num % 2 == 0 && num % 9 != 0){
-            var caja = document.createElement('div');
-            caja.setAttribute('class','blanco');
-            tablero.appendChild(caja);
+            setTimeout(() => {
+                    var caja = document.createElement('div');
+                    caja.setAttribute('class','blanco');
+                    tablero.appendChild(caja);
+            }, num * 100)
         }else if(num % 2 == 1 && num % 9 != 0){
-            var caja = document.createElement('div');
-            caja.setAttribute('class','negro');
-            tablero.appendChild(caja);
+            setTimeout(() => {
+                var caja = document.createElement('div');
+                caja.setAttribute('class','negro');
+                tablero.appendChild(caja);
+            }, num * 100)
         }else{
-            var caja = document.createElement('div');
-            caja.setAttribute('class','salto');
-            tablero.appendChild(caja);
+            setTimeout(() => {
+                var caja = document.createElement('div');
+                caja.setAttribute('class','salto');
+                tablero.appendChild(caja);
+            }, num * 100)
         }
         num++;
     };
 };
 
-var btnTablero = document.getElementById('btnTablero');
+setInterval('crearTablero()',1000);
 btnTablero.addEventListener('click', crearTablero);
